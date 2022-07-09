4d75a7aa361b6ee9391ee76dadbbd7b5ad9feb56
Sandeep Tadepalli <sandeepteja1996@gmail.com>
2022-07-08 23:59:15 -0500

    [BF - 009] Add Menu Items

--- app/src/main/java/com/sandeept/brainfkinterpreter/MainActivity.java
+++ app/src/main/java/com/sandeept/brainfkinterpreter/MainActivity.java
@@ -1,15 +1,19 @@
 package com.sandeept.brainfkinterpreter;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.sandeept.brainfkinterpreter.fragments.MainFragmentAdapter;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements Toolbar.OnMenuItemClickListener {
 
     MainFragmentAdapter adapter;
     ViewPager2 viewPager2;
@@ -33,5 +37,22 @@ public class MainActivity extends FragmentActivity {
                 tab.setText("Output");
             }
         }).attach();
+
+        MaterialToolbar toolbar = findViewById(R.id.material_tool_bar);
+        toolbar.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if(item.getItemId() == R.id.settings){
+            Toast.makeText(this, "Settings!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if(item.getItemId() == R.id.about){
+            Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return false;
     }
 }
